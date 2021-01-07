0289c25713088a6396d5e0ae1d864d2074fcf5e4
MrzvUz <mrzv.uz@gmail.com>
2021-01-07 09:01:27 -0600

    input method and counting the length of the string exercise.

--- day-1/day-1-2-exercise.py
+++ day-1/day-1-2-exercise.py
@@ -1,9 +1,14 @@
-Day 1 - String Manipulation
-String Concatenation is done with the "+" sign.
-e.g. print("Hello " + "world"
-New lines can be created with a backslash and n.
+#First *fork* your copy. Then copy-paste your code below this line 👇
+#Finally click "Run" to execute the tests
+
+print(Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+  print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n.")
+
 
 #Solution.
+
 #1. Missing double quotes before the word Day.
 print("Day 1 - String Manipulation")
 
---
+++ day-1/day-1-3-exercise.py
@@ -0,0 +1,22 @@
+print('Hello ' + input('What is your name?: ') + "!")
+print(len('Hello ' + input('What is your name?: ') + "!"))
+
+
+def test_func():
+# First *fork* your copy. Then copy-paste your code below this line 👇
+# Finally click "Run" to execute the tests
+
+
+# Write your code above this line 👆
+
+# Solution.
+# This code prints the number of characters in a user's name.
+print(len(input("What is your name? ")))
+
+# Notes
+# If input was "Jack"
+# 1st: print(len("Jack"))
+# 2nd: print(4)
+
+
+# Write your code above this line 👆
