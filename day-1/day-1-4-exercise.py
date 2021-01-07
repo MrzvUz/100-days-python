ac8b99cddf6919776fd86aeb168993963e979fbc
MrzvUz <mrzv.uz@gmail.com>
2021-01-07 10:07:42 -0600

    changing variables with switching the outputs of variables

---
+++ day-1/day-1-4-exercise.py
@@ -0,0 +1,28 @@
+# Change the output of variable a to b and b to be change to a variable without changing the code.
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+# Solution:
+
+c = a
+a = b
+b = c
+
+# Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
+
+# This is not concerned to above exercise.
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+print("Hello " + input("What is your name?"))
