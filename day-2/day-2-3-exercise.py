af27d088c779880eb3e43cd041f04edfd6e46a8a
MrzvUz <mrzv.uz@gmail.com>
2021-01-08 12:24:39 -0600

    third exercise: find out how many months, weeks, and days you have left to live

---
+++ day-2/day-2-3-exercise.py
@@ -0,0 +1,6 @@
+# https://repl.it/@appbrewery/day-2-3-exercise#README.md
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?: ")
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
