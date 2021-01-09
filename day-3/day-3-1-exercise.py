d46a9e87b7a7d5eeecfff15b500cb86093a339b5
MrzvUz <mrzv.uz@gmail.com>
2021-01-09 10:42:54 -0600

    1st Exersice with the solution

--- day-3/day-3-1-exercise.py
+++ day-3/day-3-1-exercise.py
@@ -17,3 +17,17 @@ if pick_num % 2 == 0:
     print("Your number is even.")
 else:
     print("Your number is odd number")
+
+
+# Solution: https://repl.it/@appbrewery/day-3-1-solution#main.py
+
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+# If the number can be divided by 2 with 0 remainder.
+if number % 2 == 0:
+    print("This is an even number.")
+# Otherwise (number cannot be divided by 2 with 0 remainder).
+else:
+    print("This is an odd number.")
