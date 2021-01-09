27e93c3686ad916464b3df5729946dee772a17c3
MrzvUz <mrzv.uz@gmail.com>
2021-01-09 10:28:21 -0600

    Conditional statement exercices

---
+++ day-3/day-3-1-exercise.py
@@ -0,0 +1,19 @@
+# # Control Flow with if/else and Conditional Operators
+
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height ub cm?"))
+
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+
+# Odd or Even? Exercise: https://repl.it/@appbrewery/day-3-1-exercise#README.md
+
+pick_num = int(input("Which number do you want to check?: "))
+
+if pick_num % 2 == 0:
+    print("Your number is even.")
+else:
+    print("Your number is odd number")
