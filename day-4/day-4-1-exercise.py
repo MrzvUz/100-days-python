1ec677b0894a0ba6e878d39f7081add31467ca2d
MrzvUz <mrzv.uz@gmail.com>
2021-01-19 09:45:03 -0600

    updated day-4-exersice-1

--- day-4/day-4-1-exercise.py
+++ day-4/day-4-1-exercise.py
@@ -9,14 +9,24 @@
 # random_decimal = random.random() * 5
 # print(random_decimal)
 
-# Exercise:
+# Exercise: https://repl.it/@appbrewery/day-4-1-exercise#README.md
 # Write your code below this line 👇
 # Hint: Remember to import the random module first. 🎲
 
+# import random
+
+# Heads = input("Heads or Tails?: ")
+# randomHeads = random.randint(0, 1)
+# user_choice = int(f"{randomHeads}")
+# print(user_choice)
+# print(randomHeads)
+
+# Correct Solution: https://repl.it/@appbrewery/day-4-1-solution#main.py
+
 import random
 
-Heads = input("Heads or Tails?: ")
-randomHeads = random.randint(0, 1)
-user_choice = int(f"{randomHeads}")
-print(user_choice)
-print(randomHeads)
+random_side = random.randint(0, 1)
+if random_side == 1:
+    print("Heads")
+else:
+    print("Tails")
