67b7046299ff02dcb315bc1d13e971a875520707
MrzvUz <mrzv.uz@gmail.com>
2021-01-19 09:35:23 -0600

    day-4-exersice-1

--- day-4/day-4-1-exercise.py
+++ day-4/day-4-1-exercise.py
@@ -1,4 +1,22 @@
+# import random
+
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+
+# random_float = random.random()
+# print(random_float)
+
+# random_decimal = random.random() * 5
+# print(random_decimal)
+
+# Exercise:
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+
 import random
 
-random_integer = random.randint(1, 10)
-print(random_integer)
+Heads = input("Heads or Tails?: ")
+randomHeads = random.randint(0, 1)
+user_choice = int(f"{randomHeads}")
+print(user_choice)
+print(randomHeads)
